099036ed9f96b4433506c4f838139215a7195418
liangyong <liangyong@rrkd.cn>
2017-07-24 17:33:03 +0800

    测试

---
+++ src/model/Base.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 基础支持
+ * Created by PhpStorm.
+ * User: xialiangyong
+ * Date: 2017/7/24
+ * Time: 17:09
+ */
+
+namespace wechatsdk\model;
+
+
+use wechatsdk\Wechat;
+
+class Base
+{
+    /**
+     * 获取accessToken
+     */
+    public function getAccessToken($status = false)
+    {
+
+    }
+
+}
---
+++ test/test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xialiangyong
+ * Date: 2017/7/19
+ * Time: 10:28
+ */
+require_once dir(__DIR__).'/../';
